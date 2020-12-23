753c6a14b82754de41a2548b2de94dc4a68f8875
mina <mina3504ever@gmail.com>
2020-12-23 15:57:46 -0800

    My leap year project

---
+++ .vscode/Leap_year .py
@@ -0,0 +1,11 @@
+year = int(input('What is the year you want to check for: '))
+if (year % 4) == 0:
+   if (year % 100) == 0:
+       if (year % 400) == 0:
+           print("{0} is a leap year".format(year))
+       else:
+           print("{0} is not a leap year".format(year))
+   else:
+       print("{0} is a leap year".format(year))
+else:
+   print("{0} is not a leap year".format(year))
